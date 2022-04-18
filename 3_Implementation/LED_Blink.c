fada463803e7bd524157bfeec8b30769f9aeae1e
VISHNUAMMU5140 <vs6562@srmist.edu.in>
2022-04-18 16:25:06 +0530

    commit

---
+++ 3_Implementation/LED_Blink.c
@@ -0,0 +1,28 @@
+#include "project_config.h"
+#include "user_utils.h"
+#include "LEDBlink.h"
+
+void peripheral_init(void)
+{
+	DDRB |= (1 << DDB0);
+}
+
+void change_led_state(uint8_t state)
+{
+	LED_PORT = (state << LED_PIN);
+}
+
+int main(void)
+{
+	peripheral_init();
+
+	for(;;)
+	{
+        change_led_state(LED_ON);
+		delay_ms(LED_ON_TIME);
+		
+        change_led_state(LED_OFF);
+		delay_ms(LED_OFF_TIME);	
+	}
+	return 0;
+}
